f382cb3c13567acb497eeaf759be23fbec7f7da8
Niko Cruz <francisnikocruz@gmail.com>
2019-09-27 17:44:45 -0700

    first draft of data interaction

--- {{cookiecutter.directory_name}}/app.py
+++ {{cookiecutter.directory_name}}/app.py
@@ -7,40 +7,99 @@ import pandas as pd
 import datetime
 from plotly import tools
 
+
+
+# USE ONLY FOR EXAMPLE. DELETE WHEN ADDING OWN DATA
+import numpy as np
+N = 100
+random_x = np.linspace(0, 1, N)
+random_y0 = np.random.randn(N) 
+random_y1 = np.random.randn(N)
+random_y2 = np.random.randn(N)
+
+# LOAD DATA
+# ==========
+prose_df = prose_df = pd.read_json("data/prose.json").T
+
 # DATA INTERACTION
 # ==========
+'''
+Steps for data interaction:
+    1. Choose type of graph object. (go.Scatter/go.Bar etc)
+    2. Input x data, and y data and store into variable
+    3. Group plots into an arrays for easy access
+'''
+example_scatter1 = go.Scatter(x=random_x, y=random_y0,
+                    mode='lines',
+                    name='lines',
+                    line = dict(color = '#90DAB5'),
+                    opacity = 0.8)
+
+example_scatter2 = go.Scatter(x=random_x, y=random_y1,
+                    mode='lines+markers',
+                    name='lines+markers',
+                    line = dict(color = '#3F94AB'),
+                    opacity = 0.8)
+
+example_scatter3 = go.Scatter(x=random_x, y=random_y2,
+                    mode='lines', 
+                    name='lines',
+                    line = dict(color = '#6285B2'),
+                    opacity = 0.8)
+
+examplelayout = go.Layout(
+    hovermode = "x",
+    title = "Example Title",
+    xaxis = dict(
+        range = [0,1]
+    ),
+    yaxis = dict(
+        autorange = True
+    )
+)
+
+examples = [example_scatter1, example_scatter2, example_scatter3]
 
 # APP COMPONENTS
 # ==========
+header = dbc.Jumbotron(
+    [
+        html.H1("{{cookiecutter.article_title}}", className="display-3"),
+        html.P(
+            "{{cookiecutter.article_subtitle}}"
+        )
+    ],
+    className = 'my-div text-center',
+)
+
+
+examplefig = go.Figure(data=examples, layout=examplelayout)
 
 # APP LAYOUT
 # ==========
 
-app = dash.Dash(__name__, external_scripts=['https://raw.githubusercontent.com/robin-dela/hover-effect/master/example/js/three.min.js','https://raw.githubusercontent.com/robin-dela/hover-effect/master/example/js/TweenMax.min.js','https://raw.githubusercontent.com/robin-dela/hover-effect/master/dist/hover-effect.umd.js'], external_stylesheets=[dbc.themes.BOOTSTRAP, 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+app = dash.Dash(__name__, external_scripts=['https://raw.githubusercontent.com/robin-dela/hover-effect/master/example/js/three.min.js','https://raw.githubusercontent.com/robin-dela/hover-effect/master/example/js/TweenMax.min.js','https://raw.githubusercontent.com/robin-dela/hover-effect/master/dist/hover-effect.umd.js'], external_stylesheets=[dbc.themes.BOOTSTRAP
 ])
 
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello World'),
+    header,
+    
+    dbc.Container(children =[ 
+        html.H2(prose_df.loc["intro", "title"]),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+        html.P(prose_df.loc["intro", "prose_1"]),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+        dcc.Graph(
+            figure = examplefig
+        ),
+        html.P(prose_df.loc["data explanation", "prose_1"]),
+        html.P(prose_df.loc["data explanation", "prose_2"]),
+        html.P(prose_df.loc["data explanation", "prose_3"]),
+
+    ]
+)])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ {{cookiecutter.directory_name}}/assets/custom-script.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+  parent: document.querySelector('.my-div'),
+  intensity: 0.3,
+  image1: 'https://tympanus.net/Development/DistortionHoverEffect/img/Img21.jpg',
+  image2: 'https://tympanus.net/Development/DistortionHoverEffect/img/Img22.jpg',
+  displacementImage: 'https://tympanus.net/Development/DistortionHoverEffect/img/displacement/8.jpg'
+});
